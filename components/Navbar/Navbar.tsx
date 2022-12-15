a30aa504479e3eff0768d11cc812c6ac8c2c7c1a
cdsiren <charlie@decent.xyz>
2022-12-15 16:50:08 -0500

    swap logo

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -28,7 +28,7 @@ const Navbar = () => {
   return (
     <>
       <nav className={`${styles.navbar} w-full flex flex-wrap items-center sm:justify-between justify-center`} >
-        <NavItem href="http://decent.xyz/" openInNewTab><Image width={100} height={40} src="/images/decent.png" alt="decent" /></NavItem>  
+        <NavItem href="http://burble.it" openInNewTab><Image width={20} height={20} src="/images/burble-icon.png" alt="decent" /></NavItem>  
         <LoginButton />
       </nav>
     </>
